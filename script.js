73d2acdca99dc673c791f751317996acec7680de
Rohan <nickeyjha2@gmail.com>
2022-09-05 17:03:23 +0530

    fixed issues

--- script.js
+++ script.js
@@ -26,11 +26,22 @@ function push(q){
     bottomDisplay.textContent=sum;
 }
 function half(operator){
-    
-
-    let y=bottomDisplay.textContent;
-    bottomDisplay.textContent='';
-    topDisplay.textContent=y+operator;
+    if(topDisplay.textContent!=''){
+        let x=parseInt(bottomDisplay.textContent);
+        let y=topDisplay.textContent
+        let len=y.length;
+        let z=parseInt(y.substr(0,len-1));
+        let op=y.substr(len-1,len);
+        let ans=operate(op,z,x);
+        bottomDisplay.textContent='';
+        topDisplay.textContent=ans+operator;
+    }
+    else{
+        let y=bottomDisplay.textContent;
+        bottomDisplay.textContent='';
+        topDisplay.textContent=y+operator;
+        numArray.length=0;
+    }
     numArray.length=0;
 }
 buttons=document.querySelectorAll('button')
