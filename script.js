a8dcd0c8b54af6ab02aab00afe6bbf46538f3055
Rohan <nickeyjha2@gmail.com>
2022-09-05 16:55:18 +0530

    basic function done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 topDisplay=document.querySelector('#top')
 bottomDisplay=document.querySelector('#bottom')
 
@@ -6,23 +7,58 @@ function operate(operator,num1,num2){
         return num1+num2;
     else if(operator=='*')
         return num1*num2;
-    else if(operate=='-')
+    else if(operator=='-')
         return num1-num2;
     else return num1/num2;
 
 }
-const numArray=[];
+let numArray=[];
+let operation;
 
 
+function push(q){
+    numArray.push(q);
+    let l =numArray.length;
+    let sum=0;
+    for(let k=0;k<l;k++){
+        sum=sum+numArray[k]*Math.pow(10,l-k-1);
+    }
+    bottomDisplay.textContent=sum;
+}
+function half(operator){
+    
+
+    let y=bottomDisplay.textContent;
+    bottomDisplay.textContent='';
+    topDisplay.textContent=y+operator;
+    numArray.length=0;
+}
 buttons=document.querySelectorAll('button')
 for(let i=0;i<buttons.length;i++){
     buttons[i].addEventListener('click',function(){
         let text=buttons[i].textContent;
-        if(text=='CLEAR'||text=="DELETE"){
-            bottomDisplay.textContent="";
+        if(buttons[i].id=='clear'||buttons[i].id=='delete'){
+            topDisplay.textContent='';
+            bottomDisplay.textContent='';
         }
-        else{
-            bottomDisplay.textContent=text;
+
+        else if(buttons[i].className=='num'){
+            push(buttons[i].textContent);
         }
+        else if(buttons[i].className=='operator'){
+            half(buttons[i].textContent)
+        }
+        else if(buttons[i].className=='solve'){
+            let x=parseInt(bottomDisplay.textContent);
+            let y=topDisplay.textContent
+            let len=y.length;
+            let z=parseInt(y.substr(0,len-1));
+            let op=y.substr(len-1,len);
+            let ans=operate(op,z,x);
+            topDisplay.textContent='';
+            bottomDisplay.textContent=ans;
+            numArray.length=0;
+        }
+
     })
 }
